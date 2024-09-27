a9c6c4194d6147d3a136e310619c9c637f6dd750
Marcin Floryan <marcin.floryan@gmail.com>
2024-09-27 21:28:09 +0200

    Use requests to pull tibber graphQL data

--- tibber_cost.py
+++ tibber_cost.py
@@ -1,7 +1,38 @@
 import os
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
 tibber_token = os.getenv('TIBBER_TOKEN')
 house_id = os.getenv('HOUSE_ID')
+
+TIBBER_API = "https://api.tibber.com/v1-beta/gql"
+
+query = """
+{
+  viewer {
+    home(id: "%s") {
+      currentSubscription {
+        priceInfo {
+          range(resolution: HOURLY, last: 200) {
+            nodes {
+              startsAt
+              total
+            }
+          }
+        }
+      }
+    }
+  }
+}
+""" % house_id
+
+response = requests.post(
+    TIBBER_API,
+    json={'query': query},
+    headers={
+        'Authorization': 'Bearer ' + tibber_token
+    })
+
+print(response.json())
