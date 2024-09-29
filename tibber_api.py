5c0e53775ba7ca848a200ee1bf842918c7b88a11
Marcin Floryan <marcin.floryan@gmail.com>
2024-09-29 21:57:32 +0200

    Tibber API into its own file
    Making the main file more readable and avoiding some variable name clashes

---
+++ tibber_api.py
@@ -0,0 +1,55 @@
+"""Tibber API"""
+
+from datetime import datetime, timedelta, timezone
+from requests_cache import CachedSession
+
+TIBBER_API = "https://api.tibber.com/v1-beta/gql"
+
+
+def load_query_from_file(filename):
+    with open(filename, 'r', encoding="utf-8") as file:
+        return file.read()
+
+
+def get_price_history(tibber_token, house_id):
+    query_template = load_query_from_file('tibber_price_info.graphql')
+
+    session = CachedSession('http_cache',
+                            backend='filesystem',
+                            serializer='json',
+                            allowable_methods=('GET', 'POST'))
+
+    all_data = []
+    cursor = ""
+    cutoff_date = datetime.now(timezone.utc).astimezone() - timedelta(weeks=4)
+
+    while True:
+
+        query = query_template % (house_id, cursor)
+
+        response = session.post(
+            TIBBER_API,
+            json={'query': query},
+            headers={
+                'Authorization': 'Bearer ' + tibber_token
+            },
+            timeout=0.5)
+
+        data = (response.json()['data']
+                ['viewer']['home']['currentSubscription']
+                ['priceInfo']['range'])
+
+        all_data.extend(data['nodes'])
+
+        date_of_earliest_price = datetime.fromisoformat(
+            data['nodes'][0]['startsAt'])
+
+        if date_of_earliest_price < cutoff_date:
+            break
+
+        if data['pageInfo']['hasPreviousPage']:
+            cursor = data['pageInfo']['startCursor']
+        else:
+            break
+
+    return all_data
